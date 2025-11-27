caa16114cedb40e76c2f262cba4abce7bcbc4681
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-11-27 08:49:59 +0100

    path the name of  endpoint

--- src/main/java/org/example/bloomberg/controller/FxDealController.java
+++ src/main/java/org/example/bloomberg/controller/FxDealController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/deals/import")
 @RequiredArgsConstructor
 @Validated
 public class FxDealController {
